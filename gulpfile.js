eee700adc1a74d29158c3365a9fcb3702637f176
bassam.mgm@gmail.com <bassam.mgm@gmail.com>
2020-04-23 19:39:09 +0400

    https://github.com/bassamxm/ecommerce.git

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
+var flipper = require('gulp-css-flipper');
+var merge = require('merge-stream');
 
 gulp.task('default', ['build', 'connect', 'watch']);
 
@@ -21,14 +23,17 @@ gulp.task('html', function(){
 });
 
 gulp.task('css', function() {
-    return gulp.src('src/css/*.css')
+    var flip = gulp.src(['src/css/*.css', '!src/css/style.css']).pipe(flipper());
+    var noflip = gulp.src('src/css/style.css');
+    
+    return merge(flip, noflip)
     .pipe(gulp.dest('build/css')).pipe(connect.reload());
 });
 
 gulp.task('js', function() {
     return gulp.src('src/js/*.js')
     .pipe(gulp.dest('build/js')).pipe(connect.reload());
-})
+});
 
 gulp.task('fonts', function() {
     return gulp.src('src/fonts/*')
@@ -36,7 +41,7 @@ gulp.task('fonts', function() {
 });
 
 gulp.task('images', function() {
-    return gulp.src('/src/images/*')
+    return gulp.src('src/images/*')
     .pipe(gulp.dest('build/images')).pipe(connect.reload());
 });
 
